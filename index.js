b8973b7e6b6f76b8e478b7ac41cd639078a20973
ramadan82 <ramadan.isa@gmail.com>
2022-10-15 08:45:06 +0100

    modified index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const serviceroutes = require("./src/routes/serviceroutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI, {}).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
   app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
   });
