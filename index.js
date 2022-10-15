8f27b746e757279ad1f9c12a81a2d57c71625747
ramadan82 <ramadan.isa@gmail.com>
2022-10-15 08:00:59 +0100

    removed params

--- index.js
+++ index.js
@@ -5,18 +5,13 @@ const authroutes = require("./src/routes/authroutes");
 const serviceroutes = require("./src/routes/serviceroutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-      const port = process.env.PORT;
-      console.log(`server listening on port ${port}`);
-    });
+mongoose.connect(process.env.MONGODB_URI, {}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
   });
+});
 app.use(express.json());
 app.use("/user", authroutes);
 app.use("/services", serviceroutes);
